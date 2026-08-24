dde60bbd7dd8d12312b409c9bb383280b49a3384
hemavathi94 <thayuruhemavathi@gmail.com>
2026-08-24 14:27:01 +0530

    Update application

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Kubernetes! 🚀"
+    return "This is my first Kubernetes project! 🚀"
 
 @app.route("/health")
 def health():
